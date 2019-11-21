da9336d3ebbdf2da7c3c2a23c70991e82f444f60
Melissa Castorena <mcastorena0316@gmail.com>
2019-11-21 12:29:49 -0600

    fixing rubocop mistakes

--- test.rb
+++ test.rb
@@ -1,21 +1,22 @@
 # Bubble sort
+# frozen_string_literal: true
+
+# rubocop:disable MethodLength
 def bubble_sort(arr)
   temporal = true
   i = arr.length
-
   while temporal
     temporal = false
-    (i-1).times do |x|
-      if arr[x] > arr[x+1]
-        arr[x], arr[x+1] = arr[x+1],arr[x]
-        temporal=true
+    (i - 1).times do |x|
+      if arr[x] > arr[x + 1]
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        temporal = true
       end
     end
   end
   print arr
 end
-bubble_sort([4,3,78,2,0,2])
-
+bubble_sort([4, 3, 78, 2, 0, 2])
 
 # Bubble sort yield
 
@@ -33,6 +34,7 @@ def bubble_sort_by(arr)
   end
   print arr
 end
+# rubocop: enable MethodLength
 
 bubble_sort_by(%w[hi hey hello]) do |left, right|
   left.length <=> right.length
