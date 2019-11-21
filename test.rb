05f59dabf8c1298f7140b866000098664ba0aff6
Luzaks <kanemekanik@gmail.com>
2019-11-21 11:56:32 -0600

    bubble_sort_by method builded using a yield for swtiching array elements

--- test.rb
+++ test.rb
@@ -1,38 +1,39 @@
-#Bubble sort
+# Bubble sort
 def bubble_sort(arr)
-temporal=true
+  temporal = true
+  i = arr.length
 
-i = arr.length
-
-  while temporal do 
-    temporal=false
-    (i-1).times do |x| 
+  while temporal 
+    temporal = false
+    (i-1).times do |x|
       if arr[x] > arr[x+1]
-      arr[x], arr[x+1] = arr[x+1],arr[x]
-      temporal=true
-      end 
+        arr[x], arr[x+1] = arr[x+1],arr[x]
+        temporal=true
+      end
+    end
+  end
+  print arr
+end
+bubble_sort([4,3,78,2,0,2])
+
+
+# Bubble sort yield
+
+def bubble_sort_by(arr)
+  temporal = true
+  while temporal
+    temporal = false
+    (arr.length - 1).times do |x|
+      temporal2 = yield arr[x], arr[x + 1]
+      if temporal2.positive?
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        temporal = true
+      end
     end
   end
   print arr
 end
 
-
-bubble_sort([4,3,78,2,0,2])
-
-#Bubble sort yield
-
-def bubble_sort_by(arr)
-  temporal = false
-    (arr.length - 1).times do |x|
-      temporal_2 = yield arr[x], arr[x + 1]
-      if temporal_2 > 0
-        arr[x], arr[x + 1] = arr[x + 1], arr[x]
-        temporal = true
-      end
-    end
-  print arr
-end
-
-bubble_sort_by(%w[hi hello hey]) do |left, right|
+bubble_sort_by(%w[hi hey hello]) do |left, right|
   left.length <=> right.length
 end
